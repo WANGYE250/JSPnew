9d1f0d215cd55e954e9df8f3e7982befcebc60e5
in-Solitude <AlexMercer519@outlook.com>
2023-06-12 21:07:14 +0800

    1.01,修改loginServlet的数据库错误

--- src/main/java/com/example/jspnew/LoginServlet.java
+++ src/main/java/com/example/jspnew/LoginServlet.java
@@ -17,7 +17,7 @@ public class LoginServlet extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;DatabaseName=TestSystem;trustServerCertificate=true";
+    private static final String DB_URL = "jdbc:sqlserver://localhost:3066;databaseName=TestSystem;trustServerCertificate=true";
     private static final String DB_USERNAME = "sa";
     private static final String DB_PASSWORD = "sjk12345";
 
@@ -62,25 +62,23 @@ public class LoginServlet extends HttpServlet {
         Connection connection = null;
         PreparedStatement statement=null;
         ResultSet resultSet=null;
-        try{
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
         try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
             System.out.println("正常连接数据库");
         }
-        catch(SQLException e) {
+        catch(SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         try {
 
-            String sql = "SELECT * FROM users WHERE name = ? AND pwd = ?";
+            String sql = "SELECT * FROM TestSystem.dbo.Administrator WHERE name = ? AND password = ?";
+
             if (connection != null) {
                 statement = connection.prepareStatement(sql);
             }
+
             if (statement != null) {
                 statement.setString(1, username);
             }
